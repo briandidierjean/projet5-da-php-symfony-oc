ebd4dab04d9ed34b996dd7857ef101b475a7fa65
Brian Didierjean <contact@briandidierjean.com>
2020-09-09 19:16:28 +0200

    feat: Create a PDO DAO
    This creates a PDOFactory class to access
    a MySQL database and a database.yaml to read
    the database credentials from.

---
+++ lib/core/PDOFactory.php
@@ -0,0 +1,20 @@
+<?php
+namespace Core;
+
+class PDOFactory
+{
+    public static function getMysqlConnexion()
+    {
+        $config = yaml_parse_file(__DIR__.'/config/database.yaml');
+
+        $db = new \PDO(
+            'mysql:host='.$config['host'].';
+            dbname='.$config['name'].';charset=utf8',
+            $config['user'],
+            $config['password']
+        );
+        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+        return $db;
+    }
+}
