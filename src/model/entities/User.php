3a9b28c8937e2e627f6a2ad83b4398bf81394040
Brian Didierjean <bdidierjean@2m-advisory.fr>
2020-09-21 15:10:27 +0200

    feat: Add User entity
    This creates a user entity class that represents
    a user.

---
+++ src/model/entities/User.php
@@ -0,0 +1,82 @@
+<?php
+namespace App\Model\Entity;
+
+class User extends Entity
+{
+    protected $id;
+    protected $role;
+    protected $email;
+    protected $password;
+    protected $firstName;
+    protected $lastName;
+
+    /**
+     * This method checks if a user is valid.
+     * 
+     * @return bool
+     */
+    public function isValid()
+    {
+        return !(
+            empty($this->email) ||
+            empty($this->password) ||
+            empty($this->firstName) ||
+            empty($this->lastName)
+        );
+    }
+
+    // GETTERS
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    // SETTERS
+    public function setEmail($email)
+    {
+        if (is_string($email)) {
+            $this->email = $email;
+        }
+    }
+
+    public function setPassword($password)
+    {
+        if (is_string($password)) {
+            $this->password = $password;
+        }
+    }
+
+    public function setFirstName($firstName)
+    {
+        if (is_string($firstName)) {
+            $this->firstName = $firstName;
+        }
+    }
+
+    public function setLastName($lastName)
+    {
+        if (is_string($lastName)) {
+            $this->lastName = $lastName;
+        }
+    }
+}
