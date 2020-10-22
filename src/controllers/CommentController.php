8647cb9b407335eafa584de489137a39f3bbafa7
Brian Didierjean <contact@briandidierjean.com>
2020-10-22 19:07:48 +0200

    feat: Add comment controller

---
+++ src/controllers/CommentController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+use \Core\Controller;
+use \Core\HTTPRequest;
+use \Core\HTTPResponse;
+use \App\Model\Entity\Comment;
+
+class CommentController extends Controller
+{
+    
+}
